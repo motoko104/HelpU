6d51b65dba1b13841d6cd23df14c967e9acac58c
motoko104 <motoko104@yahoo.com>
2019-10-15 21:26:48 -0500

    starting with some testing to render a part of App.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { ScrollIcon } from 'react-transformicons';
+import Jumbotron from 'react-bootstrap/Jumbotron';
+import Container from 'react-bootstrap/Container';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Jumbotron fluid>
+        <Container>
+          <h1>Help U</h1>
+          <p>
+           Let me Help you
+          </p>
+          <ScrollIcon animation="butterfly" />
+        </Container>
+      </Jumbotron>
     </div>
   );
 }
